e29e5a28332cc6a4d536495e621eb3348898b14e
ada10fl2 <ada10fl2@student.lu.se>
2014-02-19 18:53:26 +0100

    Updated Deploy

--- deploy.php
+++ deploy.php
@@ -2,10 +2,10 @@
  
 // Use in the "Post-Receive URLs" section of your GitHub repo.
  
-if ( $_POST['payload'] ) {
+if ( isset($_POST['payload']) $_POST['payload'] ) {
 
 	$cmd = "ssh-add /c/Users/ur/.ssh/deploy && cd  /c/database-proj-xampp/htdocs && git pull";
 
-	shell_exec("ssh-agent bash -c '$cmd'");
+	echo shell_exec("ssh-agent bash -c '$cmd'");
 }
-?>hi
+?>
