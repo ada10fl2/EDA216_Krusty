2f76cf35d1010a3e2ec6bcff2cd101f24b2daa21
Filip Lindqvist <buggfille@gmail.com>
2014-02-19 19:18:36 +0100

    Added some echo text

--- deploy.php
+++ deploy.php
@@ -2,10 +2,11 @@
  
 // Use in the "Post-Receive URLs" section of your GitHub repo.
  
-if ( isset($_POST['payload']) && $_POST['payload'] ) {
+if ( isset($_POST['payload']) && is_object($_POST['payload']) ) {
 
 	$cmd = "ssh-add /c/Users/ur/.ssh/deploy && cd  /c/database-proj-xampp/htdocs && git pull origin master";
-
+        echo "Runnning...";
 	echo shell_exec("ssh-agent bash -c '$cmd'");
+        echo "Done!";
 }
 ?>
