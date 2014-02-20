88fc5e16c83958f218d2204041a0ed1e8138e5f5
Filip Lindqvist <buggfille@gmail.com>
2014-02-20 16:44:47 +0100

    Updated ignore and deploy

--- deploy.php
+++ deploy.php
@@ -1,8 +1,7 @@
 <?php
- 
 // Use in the "Post-Receive URLs" section of your GitHub repo.
 $cmd = "ssh-add /c/Users/ur/.ssh/deploy; cd /c/database-proj-xampp/htdocs; git pull origin master";
 echo "Runnning...\n<br>";
 echo shell_exec("ssh-agent bash -c '$cmd'");
-echo "Done!";
+echo "...Done!";
 ?>
