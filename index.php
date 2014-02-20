3f30599dc698bb8fded911dd42e76bd2a48b53a3
Filip Lindqvist <buggfille@gmail.com>
2014-02-20 17:14:40 +0100

    Added start page

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Krusty</title>
+    </head>
+    <body>
+        <?php
+            echo date("yyyy-MM-dd hh:mm");
+        ?>
+    </body>
+</html>
