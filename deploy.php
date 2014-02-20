477e4301b5b82b0e6c88f8e65594580ec09be766
ada10fl2 <ada10fl2@student.lu.se>
2014-02-20 17:26:26 +0100

    Updated Deploy

--- deploy.php
+++ deploy.php
@@ -1,7 +1,9 @@
 <?php
 // Use in the "Post-Receive URLs" section of your GitHub repo.
 $cmd = "ssh-add /c/Users/ur/.ssh/deploy; cd /c/database-proj-xampp/htdocs; git pull origin master";
-echo "Runnning...\n<br>";
-echo shell_exec("\"C:\\Program Files (x86)\\Git\\bin\\ssh-agent.exe\" bash -c '$cmd' 2>&1");
-echo "...Done!";
+echo "Runnning...\n";
+$exec = "\"C:\\Program Files (x86)\\Git\\bin\\ssh-agent.exe\" bash -c '$cmd' 2>&1";
+echo $exec;
+echo shell_exec($exec);
+echo "\n...Done!";
 ?>
