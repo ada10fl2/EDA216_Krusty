839aa164533f841a6fb10f48c4251769ba9bccdf
Filip Lindqvist <buggfille@gmail.com>
2014-02-20 17:53:06 +0100

    Tried to fix missing ssh-agent problem again again again again

--- deploy.php
+++ deploy.php
@@ -2,7 +2,9 @@
 // Use in the "Post-Receive URLs" section of your GitHub repo.
 $git = "/c/Program Files (x86)/Git/bin/git";
 $sshadd = "/c/Program Files (x86)/Git/bin/ssh-add";
-$cmd = "'$sshadd /c/Users/ur/.ssh/deploy; cd /c/database-proj-xampp/htdocs; \"$git\" pull origin master'";
+$cmd = "'\"$sshadd\" /c/Users/ur/.ssh/deploy; "
+     . "cd /c/database-proj-xampp/htdocs; "
+     . "\"$git\" pull origin master'";
 
 $agent = "C:\\Program Files (x86)\\Git\\bin\\ssh-agent.exe";
 $bash =  "C:\\Program Files (x86)\\Git\\bin\\bash.exe";
