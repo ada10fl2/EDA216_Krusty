c19ca2a38ed948406c20c456cc5002fee6627175
Richard Simko <richard.simko@icloud.com>
2014-02-24 14:27:21 +0100

    Fixed selected product

--- pallets.php
+++ pallets.php
@@ -13,11 +13,12 @@
 		<p>
 			Start date: <input type="date" name="startdate" value="<?php echo $startDate ?>"/>
 			End date: <input type="date" name="enddate" value="<?php echo $endDate ?>"/>
-			Product name: <select>
+			Product name: <select name="productname">
+			<option value="">All products</option>
 			<?php foreach ($products as $product) { ?>
-			<option value="<?php echo $product; ?>"><?php echo $product; ?></option>
+			<option value="<?php echo $product; ?>" <?php echo $product == $productName ? "selected=\"yes\"" : ""; ?>><?php echo $product; ?></option>
 			<?php } ?>
-			</select><input type="text" name="productname" value="<?php echo $productName ?>"/>
+			</select>
 			Blocked: <input type="checkbox" name="blocked" value="true" <?php echo $blocked ? "checked" : ""; ?> />
 		</p>
 		<p>
