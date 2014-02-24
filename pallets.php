5d4ee293c6688b220b39c97e0de84cfe4ce78929
Filip Lindqvist <ada10fl2@student.lu.se>
2014-02-24 10:29:01 +0100

    Added pallets in palletspage

--- pallets.php
+++ pallets.php
@@ -1,16 +1,32 @@
 <?php $page = "pallets"; include "includes/header.php"; ?>
 	<h1>Pallets</h1>
-<?php $items= array( array("name" => "pallet1", "id" => 123), array("name" => "pallet2", "id" => 1234) ); ?>
+	<?php 
+		$items= array( 
+			array("name" => "pallet1", "id" => 1, "product" => "Nut Rings"),
+			array("name" => "pallet2", "id" => 2, "product" => "Nut Rings"),
+			array("name" => "pallet3", "id" => 3, "product" => "Nut Rings"),
+			array("name" => "pallet4", "id" => 4, "product" => "Nut Rings"),
+			array("name" => "pallet5", "id" => 5, "product" => "Nut Rings"),
+			array("name" => "pallet6", "id" => 6, "product" => "Nut Rings"),
+			array("name" => "pallet7", "id" => 7, "product" => "Nut Rings") 
+		); 
+	?>
 	<table class="table table-striped">
-		<tr>
-			<th>#</th>
-			<th>Name</th>
-        </tr>
-		<?php foreach($items as $i){ ?>
-		<tr>
-			<td><?= $i['id'] ?></td>
-			<td><?= $i['name'] ?></td>
-		</tr>
-		<?php } ?>
+		<thead>
+			<tr>
+				<th>#</th>
+				<th>Name</th>
+				<th>Kakor</th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php foreach($items as $i){ ?>
+			<tr>
+				<td><?= $i['id'] ?></td>
+				<td><?= $i['name'] ?></td>
+				<td><?= $i['product'] ?></td>
+			</tr>
+			<?php } ?>
+		</tbody>
 	</table>
 <?php include "includes/footer.php"; ?>
